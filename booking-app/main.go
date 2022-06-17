adad7640130fc46c1991fa8946f1a23dafc124c2
rajan612 <rajan612@gmail.com>
2022-06-17 16:54:49 -0400

    updated the user details and variables for booking

--- booking-app/main.go
+++ booking-app/main.go
@@ -1,27 +1,24 @@
-package main
-
-import "fmt"
-
-func main() {
-
-	conferenceName := "Goooooooo Conference 🍻"
-	const conferenceTickets = 50
-	remainingTickets := 50
-
-	fmt.Printf("welcome to the our %v Booking Application. \n", conferenceName)
-
-	fmt.Printf("We have total of %v tickets and %v are the remaining tickets.\n", conferenceTickets, remainingTickets)
+fmt.Printf("We have total of %v tickets and %v are the remaining tickets.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend  🍻🍻  ")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 
 	//Asking User Details
-	fmt.Printf("what is your name?\n")
-	fmt.Scan(&userName)
+	fmt.Println("what is your first name?")
+	fmt.Scan(&firstName)
 
-	fmt.Printf("how many tickets you want to book?\n")
+	fmt.Println("what is your flast name?")
+	fmt.Scan(&lastName)
+
+	fmt.Println("what is your email?")
+	fmt.Scan(&email)
+
+	fmt.Println("how many tickets you want to book?")
 	fmt.Scan(&userTickets)
 
-	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
+	fmt.Printf("User %v booked %v tickets\n", firstName, userTickets)
+	fmt.Printf("You will receice a confirmation email on %v \n", email)
 }
